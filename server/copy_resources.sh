4d0a19dc70ee610db1623db3c665da60a7a9c69a
Filssavi <filssavi@gmail.com>
2020-09-16 05:34:39 -0700

    Worked on deployment

--- server/copy_resources.sh
+++ server/copy_resources.sh
@@ -2,17 +2,8 @@
 
 TARGET_DIR=$(pwd)
 
-cd ../../fCore_has
+cd ../../
 
-ls .
+cp -r fCore_has $TARGET_DIR
 
-cp -r includes/fcore_has $TARGET_DIR
-mkdir build_server
-cd build_server
-cmake ../
-cmake --build . -j4 --target fchas
-cp src/libfchas.so $TARGET_DIR/
-
-cd ..
-rm -r build_server
 cd $TARGET_DIR
